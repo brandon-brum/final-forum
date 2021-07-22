2077790bb7e016049a88e195c36e1961dc892439
Brandon Brum <BrandonBrum@gmail.com>
2021-07-21 18:59:09 -0600

    Initial setup and progress

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<HTML lang="en">
+<HEAD>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home - FinalForum</title>
+	<link rel="stylesheet" type="text/css" href="styles/reset.css">
+	<link rel="stylesheet" type="text/css" href="styles/index.css">
+	<link rel="stylesheet" type="text/css" href="styles/header.css">
+</HEAD>
+<BODY>
+	<?php include 'includes/header.php' ?>
+	<main>
+		<div id='pattern'>
+			<p>L
+			<div id="login">
+				<p id="loginHeader">Login</p>
+				<hr>
+				<form>
+					<label>Username:</label>
+					<input type="text" id="username" name="username">
+					<label>Password:</label>
+					<input type="password" id="password" name="password"><br>
+					<input type="submit" id="submit" value="Log In">
+				</form>
+			</div>
+		</div>
+	</main>
+</BODY>
+</HTML>
